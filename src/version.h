710f5fea1a6c0306dfa89918322a04cf9a3491b7
cool2528 <252858527@qq.com>
2025-01-27 20:43:44 +0800

    build(src):适配Mac版本使用cmake 预设构建项目

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #pragma once
 #define GDownload_VERSION_MAJOR 0
 #define GDownload_VERSION_MINOR 1
-#define GDownload_VERSION_BUILD 16
-#define GDownload_VERSION_COMMIT cfed38d
-#define GDownload_VERSION_STRING "0.1.16.cfed38d"
+#define GDownload_VERSION_BUILD 18
+#define GDownload_VERSION_COMMIT 7ecd68b
+#define GDownload_VERSION_STRING "0.1.18.7ecd68b"
