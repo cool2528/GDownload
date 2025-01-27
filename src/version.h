821c9b619f986e5c07d0530c94b0e866bf255e4f
cool2528 <252858527@qq.com>
2025-01-27 19:47:25 +0800

    build(src):使用cmake预设构建项目

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #pragma once
 #define GDownload_VERSION_MAJOR 0
 #define GDownload_VERSION_MINOR 1
-#define GDownload_VERSION_BUILD 15
-#define GDownload_VERSION_COMMIT 71c0eea
-#define GDownload_VERSION_STRING "0.1.15.71c0eea"
+#define GDownload_VERSION_BUILD 16
+#define GDownload_VERSION_COMMIT cfed38d
+#define GDownload_VERSION_STRING "0.1.16.cfed38d"
