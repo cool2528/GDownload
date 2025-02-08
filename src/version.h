fbea6ba86e1ce3c9eaad36c4d3ad32e894c6ff8c
cool2528 <252858527@qq.com>
2025-02-08 14:45:01 +0800

    chore(src):清理提交历史记录清楚重复的提交信息

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #pragma once
 #define GDownload_VERSION_MAJOR 0
 #define GDownload_VERSION_MINOR 1
-#define GDownload_VERSION_BUILD 23
-#define GDownload_VERSION_COMMIT e5be156
-#define GDownload_VERSION_STRING "0.1.23.e5be156"
+#define GDownload_VERSION_BUILD 37
+#define GDownload_VERSION_COMMIT 9d98101
+#define GDownload_VERSION_STRING "0.1.37.9d98101"
