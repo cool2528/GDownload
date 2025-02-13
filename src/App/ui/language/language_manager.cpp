040bb8ff518f1cec8cf4409308b14cd4f5458de4
cool2528 <252858527@qq.com>
2025-02-13 14:44:23 +0800

    build(src):Mac版本GitHub action 完成

--- src/App/ui/language/language_manager.cpp
+++ src/App/ui/language/language_manager.cpp
@@ -7,96 +7,103 @@
 #include "Settings/settings_manager.h"
 
 namespace gdl {
-    namespace ui {
-        namespace language {
+	namespace ui {
+		namespace language {
 
-            LanguageManager::~LanguageManager() {
-                RemoveCurrentTranslation();
-            }
+			LanguageManager::~LanguageManager() {
+				RemoveCurrentTranslation();
+			}
 
-            LanguageManager::LanguageManager(QObject* parent) : QObject(parent) {
-                translation_path_ = QDir::currentPath() + "/translations/";
-            }
+			LanguageManager::LanguageManager(QObject* parent) : QObject(parent) {
+#ifdef __APPLE__
+				const QString path = QDir::currentPath();
+				QString app_path_dir =
+					QString::fromStdString(std::filesystem::path(path.toStdString()).parent_path().string());
+				translation_path_ = app_path_dir + "/Resources/translations/";
+#elif WIN32
+				translation_path_ = QDir::currentPath() + "/translations/";
+#endif
+			}
 
-            void LanguageManager::Initialize(QQmlEngine* engine) {
-                engine_ = engine;
-                // 从设置中读取上次使用的语言
-                const auto saved_language = gdl::ui::settings::Settings::Instance().GetLanguage();
-                if (!saved_language.isEmpty()) {
-                    SwitchLanguage(saved_language);
-                }
-            }
+			void LanguageManager::Initialize(QQmlEngine* engine) {
+				engine_ = engine;
+				// 从设置中读取上次使用的语言
+				const auto saved_language = gdl::ui::settings::Settings::Instance().GetLanguage();
+				if (!saved_language.isEmpty()) {
+					SwitchLanguage(saved_language);
+				}
+			}
 
-            bool LanguageManager::SwitchLanguage(const QString& locale) {
-                if (current_language_ == locale) {
-                    return true;
-                }
+			bool LanguageManager::SwitchLanguage(const QString& locale) {
+				if (current_language_ == locale) {
+					return true;
+				}
 
-                RemoveCurrentTranslation();
+				RemoveCurrentTranslation();
 
-                if (LoadTranslation(locale)) {
-                    current_language_ = locale;
-                    // 保存语言设置
-                    gdl::ui::settings::Settings::Instance().SetLanguage(locale);
-                    return true;
-                }
+				if (LoadTranslation(locale)) {
+					current_language_ = locale;
+					// 保存语言设置
+					gdl::ui::settings::Settings::Instance().SetLanguage(locale);
+					return true;
+				}
 
-                return false;
-            }
+				return false;
+			}
 
-            QString LanguageManager::GetCurrentLanguage() const {
-                return current_language_;
-            }
+			QString LanguageManager::GetCurrentLanguage() const {
+				return current_language_;
+			}
 
-            QStringList LanguageManager::GetSupportedLanguages() const {
-                return {"en_US", "zh_CN", "zh_TW", "ja_JP", "ko_KR"};
-            }
+			QStringList LanguageManager::GetSupportedLanguages() const {
+				return {"en_US", "zh_CN", "zh_TW", "ja_JP", "ko_KR"};
+			}
 
-            bool LanguageManager::LoadTranslation(const QString& locale) {
-                if (locale == "en_US") {
-                    qApp->removeTranslator(&translator_);
-                    current_language_ = locale;
-                    if (engine_) {
-                        engine_->retranslate();
-                    }
-                    return true;
-                }
+			bool LanguageManager::LoadTranslation(const QString& locale) {
+				if (locale == "en_US") {
+					qApp->removeTranslator(&translator_);
+					current_language_ = locale;
+					if (engine_) {
+						engine_->retranslate();
+					}
+					return true;
+				}
 
-                const QString qm_file = translation_path_ + translation_prefix_ + locale + ".qm";
+				const QString qm_file = translation_path_ + translation_prefix_ + locale + ".qm";
 
-                if (translator_.load(qm_file)) {
-                    if (qApp->installTranslator(&translator_)) {
-                        current_language_ = locale;
-                        if (engine_) {
-                            engine_->retranslate();
-                        }
-                        return true;
-                    }
-                    LOG_ERR("Failed to install translator for locale: {}", locale.toStdString());
-                }
-                else {
-                    LOG_ERR("Failed to load translation file: {}", qm_file.toStdString());
-                }
+				if (translator_.load(qm_file)) {
+					if (qApp->installTranslator(&translator_)) {
+						current_language_ = locale;
+						if (engine_) {
+							engine_->retranslate();
+						}
+						return true;
+					}
+					LOG_ERR("Failed to install translator for locale: {}", locale.toStdString());
+				}
+				else {
+					LOG_ERR("Failed to load translation file: {}", qm_file.toStdString());
+				}
 
-                return false;
-            }
+				return false;
+			}
 
-            void LanguageManager::RemoveCurrentTranslation() {
-                if (!current_language_.isEmpty()) {
-                    qApp->removeTranslator(&translator_);
-                }
-            }
+			void LanguageManager::RemoveCurrentTranslation() {
+				if (!current_language_.isEmpty()) {
+					qApp->removeTranslator(&translator_);
+				}
+			}
 
-            void RegisterTypes(QQmlEngine* engine) {
-                if (!engine) {
-                    LOG_ERR("invalid QQmlEngine");
-                    return;
-                }
+			void RegisterTypes(QQmlEngine* engine) {
+				if (!engine) {
+					LOG_ERR("invalid QQmlEngine");
+					return;
+				}
 
-                engine->rootContext()->setContextProperty("LanguageManager", &LanguageManager::Instance());
-                LanguageManager::Instance().Initialize(engine);
-            }
+				engine->rootContext()->setContextProperty("LanguageManager", &LanguageManager::Instance());
+				LanguageManager::Instance().Initialize(engine);
+			}
 
-        }  // namespace language
-    }  // namespace ui
+		}  // namespace language
+	}  // namespace ui
 }  // namespace gdl
