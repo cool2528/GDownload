46e81a36974aa775f9f7782c8187bec6468851da
cool2528 <252858527@qq.com>
2025-02-13 13:46:10 +0800

    chore(src):清理mac版本GitHub action 自动构建的垃圾提交信息

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #pragma once
 #define GDownload_VERSION_MAJOR 0
 #define GDownload_VERSION_MINOR 1
-#define GDownload_VERSION_BUILD 42
-#define GDownload_VERSION_COMMIT faa7ea6
-#define GDownload_VERSION_STRING "0.1.42.faa7ea6"
+#define GDownload_VERSION_BUILD 53
+#define GDownload_VERSION_COMMIT 7be614b
+#define GDownload_VERSION_STRING "0.1.53.7be614b"
