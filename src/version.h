0d5e6e7a62e3400a3b3066cdbacb6a0c071e6817
cool2528 <252858527@qq.com>
2025-02-14 17:28:53 +0800

    build(src):修复构建打包文件输出错误

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #pragma once
-#define GDownload_VERSION_MAJOR 0
-#define GDownload_VERSION_MINOR 1
-#define GDownload_VERSION_BUILD 57
-#define GDownload_VERSION_COMMIT c5ba928
-#define GDownload_VERSION_STRING "0.1.57.c5ba928"
+#define GDownload_VERSION_MAJOR 1
+#define GDownload_VERSION_MINOR 0
+#define GDownload_VERSION_BUILD 0
+#define GDownload_VERSION_COMMIT b2e8d78
+#define GDownload_VERSION_STRING "1.0.0.b2e8d78"
