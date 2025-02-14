052b8849242f8602635de2dfc9fa2903ad7a7fb1
cool2528 <252858527@qq.com>
2025-02-14 16:08:22 +0800

    build(src):修改构建脚本

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #pragma once
 #define GDownload_VERSION_MAJOR 0
 #define GDownload_VERSION_MINOR 1
-#define GDownload_VERSION_BUILD 57
-#define GDownload_VERSION_COMMIT c5ba928
-#define GDownload_VERSION_STRING "0.1.57.c5ba928"
+#define GDownload_VERSION_BUILD 58
+#define GDownload_VERSION_COMMIT 00c9fd2
+#define GDownload_VERSION_STRING "0.1.58.00c9fd2"
