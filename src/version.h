83a98839bde23fdaad4c504640fdbe025ec51408
cool2528 <252858527@qq.com>
2025-02-25 15:24:24 +0800

    fix(src):修复打包后的appimage 启动aria2c失败

--- src/version.h
+++ src/version.h
@@ -2,5 +2,5 @@
 #define GDownload_VERSION_MAJOR 1
 #define GDownload_VERSION_MINOR 0
 #define GDownload_VERSION_BUILD 6
-#define GDownload_VERSION_COMMIT 160a4a7
-#define GDownload_VERSION_STRING "1.0.6.160a4a7"
+#define GDownload_VERSION_COMMIT e8a8e6a
+#define GDownload_VERSION_STRING "1.0.6.e8a8e6a"
