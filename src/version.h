0d4b3ec158c8c1809c4e8be0ccb7d16a9319ce9f
cool2528 <252858527@qq.com>
2025-02-25 13:50:45 +0800

    build(src):修改macos系统vcpkg 也使用动态库编译打包

--- src/version.h
+++ src/version.h
@@ -2,5 +2,5 @@
 #define GDownload_VERSION_MAJOR 1
 #define GDownload_VERSION_MINOR 0
 #define GDownload_VERSION_BUILD 6
-#define GDownload_VERSION_COMMIT 14ac722
-#define GDownload_VERSION_STRING "1.0.6.14ac722"
+#define GDownload_VERSION_COMMIT 160a4a7
+#define GDownload_VERSION_STRING "1.0.6.160a4a7"
