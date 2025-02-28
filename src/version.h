ae301bb8424513d76cc8b92868cbd005d6eabd9f
cool2528 <252858527@qq.com>
2025-02-28 11:41:09 +0800

    build(src):为Linux系统更新增加appimageupdate库

--- src/version.h
+++ src/version.h
@@ -2,5 +2,5 @@
 #define GDownload_VERSION_MAJOR 1
 #define GDownload_VERSION_MINOR 0
 #define GDownload_VERSION_BUILD 6
-#define GDownload_VERSION_COMMIT 46e9fd0
-#define GDownload_VERSION_STRING "1.0.6.46e9fd0"
+#define GDownload_VERSION_COMMIT 7522725
+#define GDownload_VERSION_STRING "1.0.6.7522725"
