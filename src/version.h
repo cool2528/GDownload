6ab24bfdb893ff124404a2416291bd9feea79241
cool2528 <252858527@qq.com>
2025-03-04 10:29:44 +0800

    build(src):fix mac 自动部署失败

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #pragma once
 #define GDownload_VERSION_MAJOR 1
 #define GDownload_VERSION_MINOR 0
-#define GDownload_VERSION_BUILD 7
-#define GDownload_VERSION_COMMIT cc9d376
-#define GDownload_VERSION_STRING "1.0.7.cc9d376"
+#define GDownload_VERSION_BUILD 8
+#define GDownload_VERSION_COMMIT 872d40a
+#define GDownload_VERSION_STRING "1.0.8.872d40a"
