f41ba580149f97b03edf175f1158d92db75bbeb9
cool2528 <252858527@qq.com>
2025-03-04 16:18:30 +0800

    build(src):mac系统下自动更新完成并且支持自动发布构建

--- src/App/ui/view/mainwindow.cxx
+++ src/App/ui/view/mainwindow.cxx
@@ -21,122 +21,122 @@
 #include "version.h"
 FRAMELESSHELPER_USE_NAMESPACE
 namespace gd {
-    namespace ui {
-        MainWindow::MainWindow(QObject* parent) : QObject(parent) {
-            InitQtMessageHandler();
-        }
-        MainWindow::~MainWindow() {}
-        int MainWindow::Exec(int argc, char* argv[]) {
+	namespace ui {
+		MainWindow::MainWindow(QObject* parent) : QObject(parent) {
+			InitQtMessageHandler();
+		}
+		MainWindow::~MainWindow() {}
+		int MainWindow::Exec(int argc, char* argv[]) {
 
-            FramelessHelper::Quick::initialize();
-            QApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
-            QApplication::setAttribute(Qt::AA_DontCreateNativeWidgetSiblings);
+			FramelessHelper::Quick::initialize();
+			QApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+			QApplication::setAttribute(Qt::AA_DontCreateNativeWidgetSiblings);
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-            QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+			QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
-            QApplication::setOrganizationName("gdl");
-            QApplication::setApplicationName("gdownload");
-            QApplication app(argc, argv);
-            InitIcon(&app);
-            QQmlApplicationEngine engine;
-            InitQmlEngine(&engine);
-            InitFont(&engine);
-            QObject::connect(
-                &engine, &QQmlApplicationEngine::objectCreationFailed, &app, []() { QApplication::exit(-1); },
-                Qt::QueuedConnection);
-            const QUrl url(QStringLiteral("qrc:/qml/mainWindow.qml"));
-            engine.addImportPath(QStringLiteral("qrc:/qml"));
-            engine.load(url);
-            const auto code = app.exec();
-            UnInitEngine();
-            return code;
-        }
+			QApplication::setOrganizationName("gdl");
+			QApplication::setApplicationName("gdownload");
+			QApplication app(argc, argv);
+			InitIcon(&app);
+			QQmlApplicationEngine engine;
+			InitQmlEngine(&engine);
+			InitFont(&engine);
+			QObject::connect(
+				&engine, &QQmlApplicationEngine::objectCreationFailed, &app, []() { QApplication::exit(-1); },
+				Qt::QueuedConnection);
+			const QUrl url(QStringLiteral("qrc:/qml/mainWindow.qml"));
+			engine.addImportPath(QStringLiteral("qrc:/qml"));
+			engine.load(url);
+			const auto code = app.exec();
+			UnInitEngine();
+			return code;
+		}
 
-        void MainWindow::InitQmlEngine(QQmlEngine* engine) {
-            gdl::ui::settings::RegisterTypes(engine);
-            qmlRegisterType<gdl::ui::models::FolderHistoryModel>("gdl.sdk", 1, 0, "FolderHistoryModel");
-            gdl::ui::language::RegisterTypes(engine);
-            FramelessHelper::Core::setApplicationOSThemeAware();
-            FramelessHelper::Quick::registerTypes(engine);
-            gdl::ui::utils::RegisterTypes(engine);
-            gdl::ui::theme::RegisterTypes(engine);
-            qmlRegisterUncreatableMetaObject(SegoeFluentIcons::staticMetaObject, GEXPORT_MODULE_URL, 1, 0,
-                                             "SegoeFluentIcons", "SegoeFluentIcons enum");
-            if (!gdl::cache::DownloadHistoryCache::Instance().Initialize(gdl::os::GetAppDataDir() +
-                                                                         "/gdownload/db/gdownload.db")) {
-                LOG_ERR("init download history cache fail")
-            }
-            gdl::ui::browser::RegisterTypes(engine);
-            gdl::ui::toast::RegisterTypes(engine);
-            gdl::update::RegisterTypes(engine);
-            gdl::update::UpdateConfig update_config;
-            update_config.current_version	 = "1.0.5";	 //GDownload_VERSION_STRING;
-            update_config.disable_auto_check = gdl::ui::settings::Settings::Instance().GetEnableAutoUpdate();
+		void MainWindow::InitQmlEngine(QQmlEngine* engine) {
+			gdl::ui::settings::RegisterTypes(engine);
+			qmlRegisterType<gdl::ui::models::FolderHistoryModel>("gdl.sdk", 1, 0, "FolderHistoryModel");
+			gdl::ui::language::RegisterTypes(engine);
+			FramelessHelper::Core::setApplicationOSThemeAware();
+			FramelessHelper::Quick::registerTypes(engine);
+			gdl::ui::utils::RegisterTypes(engine);
+			gdl::ui::theme::RegisterTypes(engine);
+			qmlRegisterUncreatableMetaObject(SegoeFluentIcons::staticMetaObject, GEXPORT_MODULE_URL, 1, 0,
+											 "SegoeFluentIcons", "SegoeFluentIcons enum");
+			if (!gdl::cache::DownloadHistoryCache::Instance().Initialize(gdl::os::GetAppDataDir() +
+																		 "/gdownload/db/gdownload.db")) {
+				LOG_ERR("init download history cache fail")
+			}
+			gdl::ui::browser::RegisterTypes(engine);
+			gdl::ui::toast::RegisterTypes(engine);
+			gdl::update::RegisterTypes(engine);
+			gdl::update::UpdateConfig update_config;
+			update_config.current_version	 = GDownload_VERSION_STRING;
+			update_config.disable_auto_check = gdl::ui::settings::Settings::Instance().GetEnableAutoUpdate();
 #if defined(_WIN32) || defined(_WIN64)
-            update_config.update_url = "https://api.github.com/repos/cool2528/gdownload/releases/latest";
-            std::map<std::string, std::string> headers;
-            headers["Content-Type"] = "application/json";
-            headers["User-Agent"]	= "GDownloader-Update-Client";
-            headers["Authorization"] =
-                "Bearer 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";
-            headers["X-GitHub-Api-Version"] = "2022-11-28";
-            headers["Accept"]				= "application/vnd.github.v3+json";
-            gdl::update::UpdateManager::Instance().SetRequestHeaders(headers);
+			update_config.update_url = "https://api.github.com/repos/cool2528/gdownload/releases/latest";
+			std::map<std::string, std::string> headers;
+			headers["Content-Type"] = "application/json";
+			headers["User-Agent"]	= "GDownloader-Update-Client";
+			headers["Authorization"] =
+				"Bearer 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";
+			headers["X-GitHub-Api-Version"] = "2022-11-28";
+			headers["Accept"]				= "application/vnd.github.v3+json";
+			gdl::update::UpdateManager::Instance().SetRequestHeaders(headers);
 #elif defined(__linux__)
 #elif defined(__APPLE__)
 #endif
-            gdl::update::UpdateManager::Instance().Initialize(update_config);
-        }
-        void MainWindow::InitTranslation(QGuiApplication* app) {}
-        void MainWindow::InitFont(QQmlEngine* engine) {
-            fluent_icons_font_id_ = QFontDatabase::addApplicationFont("://font/SegoeFluentIcons.ttf");
-            if (fluent_icons_font_id_ == -1) {
-                LOG_ERR("init fluenticons font fail");
-                return;
-            }
-            const auto font_family_name = QFontDatabase::applicationFontFamilies(fluent_icons_font_id_).at(0);
-            engine->rootContext()->setContextProperty("FluentIcons", font_family_name);
-        }
+			gdl::update::UpdateManager::Instance().Initialize(update_config);
+		}
+		void MainWindow::InitTranslation(QGuiApplication* app) {}
+		void MainWindow::InitFont(QQmlEngine* engine) {
+			fluent_icons_font_id_ = QFontDatabase::addApplicationFont("://font/SegoeFluentIcons.ttf");
+			if (fluent_icons_font_id_ == -1) {
+				LOG_ERR("init fluenticons font fail");
+				return;
+			}
+			const auto font_family_name = QFontDatabase::applicationFontFamilies(fluent_icons_font_id_).at(0);
+			engine->rootContext()->setContextProperty("FluentIcons", font_family_name);
+		}
 
-        void MainWindow::InitIcon(QGuiApplication* app) {
+		void MainWindow::InitIcon(QGuiApplication* app) {
 #if defined(_WIN32) || defined(_WIN64)
-            app->setWindowIcon(QIcon(":/images/logo/icon.ico"));
+			app->setWindowIcon(QIcon(":/images/logo/icon.ico"));
 #endif
-        }
+		}
 
-        void MainWindow::UnInitEngine() {
-            gdl::ui::browser::BrowserManager::Instance().UnInit();
-            gdl::engine::Aria2cDownloadManager::Instance().UninitAria2cEngine();
-            gdl::cache::DownloadHistoryCache::Instance().Uninitialize();
-            gdl::ui::settings::Settings::Instance().UnInit();
-        }
+		void MainWindow::UnInitEngine() {
+			gdl::ui::browser::BrowserManager::Instance().UnInit();
+			gdl::engine::Aria2cDownloadManager::Instance().UninitAria2cEngine();
+			gdl::cache::DownloadHistoryCache::Instance().Uninitialize();
+			gdl::ui::settings::Settings::Instance().UnInit();
+		}
 
-        void MainWindow::InitQtMessageHandler() const {
-            qInstallMessageHandler([](QtMsgType type, const QMessageLogContext& context, const QString& msg) {
-                QByteArray localMsg	 = msg.toLocal8Bit();
-                const char* file	 = context.file ? context.file : "";
-                const char* function = context.function ? context.function : "";
-                switch (type) {
-                    case QtDebugMsg:
+		void MainWindow::InitQtMessageHandler() const {
+			qInstallMessageHandler([](QtMsgType type, const QMessageLogContext& context, const QString& msg) {
+				QByteArray localMsg	 = msg.toLocal8Bit();
+				const char* file	 = context.file ? context.file : "";
+				const char* function = context.function ? context.function : "";
+				switch (type) {
+					case QtDebugMsg:
 #if defined(_DEBUG)
-                        LOG_DBG("Debug: {} ({}, {}, {})", localMsg.constData(), file, context.line, function)
+						LOG_DBG("Debug: {} ({}, {}, {})", localMsg.constData(), file, context.line, function)
 #endif
-                        break;
-                    case QtInfoMsg:
-                        LOG_INFO("Info: {} ({}, {}, {})", localMsg.constData(), file, context.line, function)
-                        break;
-                    case QtWarningMsg:
-                        LOG_WARN("Warning: {} ({}, {}, {})", localMsg.constData(), file, context.line, function)
-                        break;
-                    case QtCriticalMsg:
-                        LOG_CRIT("Critical: {} ({}, {}, {})", localMsg.constData(), file, context.line, function)
-                        break;
-                    case QtFatalMsg:
-                        LOG_ERR("Fatal: {} ({}, {}, {})", localMsg.constData(), file, context.line, function)
-                        break;
-                }
-            });
-        }
+						break;
+					case QtInfoMsg:
+						LOG_INFO("Info: {} ({}, {}, {})", localMsg.constData(), file, context.line, function)
+						break;
+					case QtWarningMsg:
+						LOG_WARN("Warning: {} ({}, {}, {})", localMsg.constData(), file, context.line, function)
+						break;
+					case QtCriticalMsg:
+						LOG_CRIT("Critical: {} ({}, {}, {})", localMsg.constData(), file, context.line, function)
+						break;
+					case QtFatalMsg:
+						LOG_ERR("Fatal: {} ({}, {}, {})", localMsg.constData(), file, context.line, function)
+						break;
+				}
+			});
+		}
 
-    }  // namespace ui
+	}  // namespace ui
 }  // namespace gd
--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #pragma once
 #define GDownload_VERSION_MAJOR 1
 #define GDownload_VERSION_MINOR 0
-#define GDownload_VERSION_BUILD 8
-#define GDownload_VERSION_COMMIT 872d40a
-#define GDownload_VERSION_STRING "1.0.8.872d40a"
+#define GDownload_VERSION_BUILD 9
+#define GDownload_VERSION_COMMIT 98aad40
+#define GDownload_VERSION_STRING "1.0.9.98aad40"
