a341b397d03f60607d104d70f1b22327b705b517
cool2528 <252858527@qq.com>
2025-03-30 18:57:59 +0800

    build(src):更新action token

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #pragma once
 #define GDownload_VERSION_MAJOR 1
 #define GDownload_VERSION_MINOR 0
-#define GDownload_VERSION_BUILD 1
-#define GDownload_VERSION_COMMIT 59f5473
-#define GDownload_VERSION_STRING "1.0.1.59f5473"
+#define GDownload_VERSION_BUILD 2
+#define GDownload_VERSION_COMMIT b85f9b7
+#define GDownload_VERSION_STRING "1.0.2.b85f9b7"
