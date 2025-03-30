b85f9b7f84b7ea2cd093873f66b6c360e2c886bf
cool2528 <252858527@qq.com>
2025-03-30 17:21:02 +0800

    build(ci): 修复Linux下因cpr库冲突导致编译失败

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #pragma once
 #define GDownload_VERSION_MAJOR 1
 #define GDownload_VERSION_MINOR 0
-#define GDownload_VERSION_BUILD 0
-#define GDownload_VERSION_COMMIT 70c9b66
-#define GDownload_VERSION_STRING "1.0.0.70c9b66"
+#define GDownload_VERSION_BUILD 1
+#define GDownload_VERSION_COMMIT 59f5473
+#define GDownload_VERSION_STRING "1.0.1.59f5473"
