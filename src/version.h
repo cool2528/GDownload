959b537772fe9f25052d06a378467181bc944c0b
cool2528 <252858527@qq.com>
2025-08-16 21:17:27 +0800

    ci(release): 替换RELEASE_TOKEN为GITHUB_TOKEN

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #pragma once
 #define GDownload_VERSION_MAJOR 1
 #define GDownload_VERSION_MINOR 0
-#define GDownload_VERSION_BUILD 0
-#define GDownload_VERSION_COMMIT 5e7690a
-#define GDownload_VERSION_STRING "1.0.0.5e7690a"
+#define GDownload_VERSION_BUILD 1
+#define GDownload_VERSION_COMMIT b570320
+#define GDownload_VERSION_STRING "1.0.1.b570320"
