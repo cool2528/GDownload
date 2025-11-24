330972f2674179b8a33373807c5ca3bd3dd7d3f0
cool2528 <252858527@qq.com>
2025-11-24 20:02:21 +0800

    build(vcpkg): 更新 framelesshelper 移植文件以支持 Qt 6.10
    新增多个补丁文件以修复 framelesshelper 在 Qt 6.10 环境下的兼容性问题，
    包括尺寸比较运算符、深色模式检测以及合成渲染相关的调整。同时更新版本哈希值。

--- src/version.h
+++ src/version.h
@@ -2,5 +2,5 @@
 #define GDownload_VERSION_MAJOR 1
 #define GDownload_VERSION_MINOR 0
 #define GDownload_VERSION_BUILD 5
-#define GDownload_VERSION_COMMIT 18eb699
-#define GDownload_VERSION_STRING "1.0.5.18eb699"
+#define GDownload_VERSION_COMMIT 90a116a
+#define GDownload_VERSION_STRING "1.0.5.90a116a"
