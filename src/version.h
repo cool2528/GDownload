2d4acd0b710d1b30a3072f0594a7df1d194d33eb
cool2528 <252858527@qq.com>
2025-11-24 20:41:05 +0800

    feat(config): 添加 Aria2 配置文件并更新版本信息

--- src/version.h
+++ src/version.h
@@ -2,5 +2,5 @@
 #define GDownload_VERSION_MAJOR 1
 #define GDownload_VERSION_MINOR 0
 #define GDownload_VERSION_BUILD 5
-#define GDownload_VERSION_COMMIT 90a116a
-#define GDownload_VERSION_STRING "1.0.5.90a116a"
+#define GDownload_VERSION_COMMIT 330972f
+#define GDownload_VERSION_STRING "1.0.5.330972f"
