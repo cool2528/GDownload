108af63335c1ef04b268ba75c93a0d175f82ec60
cool2528 <252858527@qq.com>
2026-06-21 09:10:37 +0800

    refactor(ui): 抽 IBrowserManager 接口

--- src/App/ui/Browser/browser_manager.h
+++ src/App/ui/Browser/browser_manager.h
@@ -1,4 +1,5 @@
-﻿#pragma once
+#pragma once
+#include "IBrowserManager.h"
 #include <QtQml/qqml.h>
 #include <QObject>
 #include <QVariantMap>
@@ -14,80 +15,83 @@ namespace gdl {
 		namespace browser {
 
 			Q_NAMESPACE
-			class BrowserManager : public QObject, public Singleton<BrowserManager> {
+			// 下载管理实现类,继承 IBrowserManager 纯虚接口与 Singleton<BrowserManagerImpl>
+			// QObject 必须置于基类列表首位,moc 依此链接 staticMetaObject;IBrowserManager 为非 Q_OBJECT 纯虚接口
+			// 保留 Q_INVOKABLE 以确保 moc 注册元对象信息,QML 侧方法调用不受影响
+			class BrowserManagerImpl : public QObject, public IBrowserManager, public Singleton<BrowserManagerImpl> {
 				Q_OBJECT
-				SINGLETON_DECLARE(BrowserManager)
+				SINGLETON_DECLARE(BrowserManagerImpl)
 				QML_SINGLETON
 			   public:
-				static BrowserManager* create(QQmlEngine*, QJSEngine*);
+				static BrowserManagerImpl* create(QQmlEngine*, QJSEngine*);
 
 			   public:
-				~BrowserManager() override;
+				~BrowserManagerImpl() override;
 
-				Q_INVOKABLE void SyncTrackersServerlist();
+				Q_INVOKABLE void SyncTrackersServerlist() override;
 
-				Q_INVOKABLE DownloadTaskModel* GetActiveDownloadModel();
+				Q_INVOKABLE DownloadTaskModel* GetActiveDownloadModel() override;
 
-				Q_INVOKABLE DownloadTaskModel* GetStopedDownloadModel();
+				Q_INVOKABLE DownloadTaskModel* GetStopedDownloadModel() override;
 
-				Q_INVOKABLE DownloadTaskModel* GetWaitingDownloadModel();
+				Q_INVOKABLE DownloadTaskModel* GetWaitingDownloadModel() override;
 
-                Q_INVOKABLE bool AddHttpTask(const QVariantList& urls, const QVariantMap& options);
+                Q_INVOKABLE bool AddHttpTask(const QVariantList& urls, const QVariantMap& options) override;
 
-                Q_INVOKABLE bool AddTorrentTask(const QString& tarrent, const QVariantMap& options);
+                Q_INVOKABLE bool AddTorrentTask(const QString& tarrent, const QVariantMap& options) override;
 
-                Q_INVOKABLE bool AddMetalinkTask(const QString& metalink, const QVariantMap& options);
+                Q_INVOKABLE bool AddMetalinkTask(const QString& metalink, const QVariantMap& options) override;
 
-                Q_INVOKABLE bool PauseTask(int page_index, const QString& gid);
+                Q_INVOKABLE bool PauseTask(int page_index, const QString& gid) override;
 
-                Q_INVOKABLE bool PauseAllTask(int page_index);
+                Q_INVOKABLE bool PauseAllTask(int page_index) override;
 
-                Q_INVOKABLE bool ForcePauseTask(int page_index, const QString& gid);
+                Q_INVOKABLE bool ForcePauseTask(int page_index, const QString& gid) override;
 
-                Q_INVOKABLE bool ForcePauseAllTask();
+                Q_INVOKABLE bool ForcePauseAllTask() override;
 
-                Q_INVOKABLE bool UnpauseTask(int page_index, const QString& gid);
+                Q_INVOKABLE bool UnpauseTask(int page_index, const QString& gid) override;
 
-                Q_INVOKABLE bool UnpauseAllTask(int page_index);
+                Q_INVOKABLE bool UnpauseAllTask(int page_index) override;
 
-                Q_INVOKABLE bool RemoveTask(int page_index, const QString& gid, bool is_remove_file = false);
+                Q_INVOKABLE bool RemoveTask(int page_index, const QString& gid, bool is_remove_file = false) override;
 
-                Q_INVOKABLE bool RemoveAllTask(int page_index, bool is_remove_file = true);
+                Q_INVOKABLE bool RemoveAllTask(int page_index, bool is_remove_file = true) override;
 
-				Q_INVOKABLE bool ForceRemoveTask(const QString& gid);
+				Q_INVOKABLE bool ForceRemoveTask(const QString& gid) override;
 
-				Q_INVOKABLE bool RemoveDownloadResult(const QString& gid);
+				Q_INVOKABLE bool RemoveDownloadResult(const QString& gid) override;
 
-				Q_INVOKABLE bool PurgeDownloadResult();
+				Q_INVOKABLE bool PurgeDownloadResult() override;
 
-				Q_INVOKABLE bool ChangeOption(const QString& gid, const QVariantMap& options);
+				Q_INVOKABLE bool ChangeOption(const QString& gid, const QVariantMap& options) override;
 
-				Q_INVOKABLE bool ChangeGlobalOption(const QVariantMap& options);
+				Q_INVOKABLE bool ChangeGlobalOption(const QVariantMap& options) override;
 
-				Q_INVOKABLE void OpenFileLocation(const QString& file_path);
+				Q_INVOKABLE void OpenFileLocation(const QString& file_path) override;
 
-                Q_INVOKABLE bool RemoveStopTask(const QString& gid, bool is_remove_file = true) const;
+                Q_INVOKABLE bool RemoveStopTask(const QString& gid, bool is_remove_file = true) const override;
 
-                Q_INVOKABLE bool RemoveStopTask(int index, bool is_remove_file = true) const;
+                Q_INVOKABLE bool RemoveStopTask(int index, bool is_remove_file = true) const override;
 
-                Q_INVOKABLE bool RemoveAllStopTask(bool is_remove_file = true) const;
+                Q_INVOKABLE bool RemoveAllStopTask(bool is_remove_file = true) const override;
 
-                Q_INVOKABLE void RefreshTaskList(int page_index);
-                Q_INVOKABLE parser::FilePreviewModel* GetFilePreviewModel(const QString& file_path);
+                Q_INVOKABLE void RefreshTaskList(int page_index) override;
+                Q_INVOKABLE parser::FilePreviewModel* GetFilePreviewModel(const QString& file_path) override;
 
 				bool Init();
 				void UnInit();
 
 			   public:
 			   Q_SIGNALS:
-				void sigErrorMessage(const QString& error);
-				void sigUpdateTasksMessage(const DownloadTaskInfo& info);
-                void sigUpdateActiveProgress(const double& progress);
-				void sigUpdateSyncServerList(const QString& list);
-				void sigTrackerUpdateStatus(const QString& status);
+				void sigErrorMessage(const QString& error) override;
+				void sigUpdateTasksMessage(const DownloadTaskInfo& info) override;
+                void sigUpdateActiveProgress(const double& progress) override;
+				void sigUpdateSyncServerList(const QString& list) override;
+				void sigTrackerUpdateStatus(const QString& status) override;
 
 			   private:
-				explicit BrowserManager(QObject* parent = nullptr);
+				explicit BrowserManagerImpl(QObject* parent = nullptr);
 				void OnHandleAria2Message(const std::string& msg);
                 void OnHandleAria2ActiveProgress(const std::string& msg);
 				void OnHandleTrackerUpdateStatus(const std::string& msg);
@@ -109,6 +113,8 @@ namespace gdl {
 				engine::Subscription aria2_sync_server_list_subcription_{nullptr};
 				engine::Subscription aria2_tracker_update_status_subscription_{nullptr};
 			};
+			// 类型别名:保 4 处现有 BrowserManager::Instance() 调用点零改动
+			using BrowserManager = BrowserManagerImpl;
 			void RegisterTypes(QQmlEngine* engine);
 		}  // namespace browser
 	}  // namespace ui
